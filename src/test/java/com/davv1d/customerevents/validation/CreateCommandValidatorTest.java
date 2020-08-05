280b89365be9b147052424914065fde4c77ec1f2
davv1d <kowalskidawid63@gmail.com>
2020-08-05 21:51:58 +0200

    Change CreateCommandValidatorTest
    Added cleanUp method and changed assertions

--- src/test/java/com/davv1d/customerevents/validation/CreateCommandValidatorTest.java
+++ src/test/java/com/davv1d/customerevents/validation/CreateCommandValidatorTest.java
@@ -1,15 +1,10 @@
 package com.davv1d.customerevents.validation;
 
 import com.davv1d.customerevents.command.CreateCommand;
-import com.davv1d.customerevents.domain.CustomerState;
-import com.davv1d.customerevents.domain.Email;
-import com.davv1d.customerevents.domain.Name;
-import com.davv1d.customerevents.repository.EmailRepository;
-import com.davv1d.customerevents.repository.NameRepository;
+import com.davv1d.customerevents.domain.*;
+import com.davv1d.customerevents.repository.*;
 import javaslang.control.Try;
-import org.junit.Assert;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.validation.BindException;
@@ -17,6 +12,8 @@ import org.springframework.validation.FieldError;
 
 import java.time.Instant;
 import java.util.UUID;
+
+import static org.junit.Assert.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CreateCommandValidatorTest {
@@ -30,110 +27,105 @@ class CreateCommandValidatorTest {
     @Autowired
     private EmailRepository emailRepository;
 
+    @AfterEach
+    public void cleanUp() {
+        nameRepository.deleteAll();
+        emailRepository.deleteAll();
+    }
+
     @Test
-    @DisplayName("should return error name exists in database")
+    @DisplayName("should return error - name exists in database")
     public void shouldReturnErrorNameExistsInDatabase() {
         //Given
         FieldError nameError = new FieldError("CreateCommand", "name", "name exists in database");
         Name name = new Name("test name");
-        Name savedName = nameRepository.save(name);
+        nameRepository.save(name);
         CreateCommand createCommand = new CreateCommand(UUID.randomUUID(), "test name", "test email", CustomerState.INITIALIZED, Instant.now());
         //When
         Try<CreateCommand> validCommand = createCommandValidator.valid(createCommand);
         //Then
-        Assert.assertTrue(validCommand.isFailure());
-        Assert.assertTrue(((BindException) validCommand.getCause()).getAllErrors().contains(nameError));
-        Assert.assertEquals(1, ((BindException) validCommand.getCause()).getFieldErrors().size());
-        //Clean up
-        nameRepository.deleteById(savedName.getId());
+        assertTrue(validCommand.isFailure());
+        assertTrue(((BindException) validCommand.getCause()).getAllErrors().contains(nameError));
+        assertEquals(1, ((BindException) validCommand.getCause()).getFieldErrors().size());
     }
 
     @Test
-    @DisplayName("should return error (letter size) name exists in database")
+    @DisplayName("should return error (letters size) - name exists in database")
     public void shouldReturnErrorIgnoreCaseNameExistsInDatabase() {
         //Given
         FieldError nameError = new FieldError("CreateCommand", "name", "name exists in database");
         Name name = new Name("TEST name");
-        Name savedName = nameRepository.save(name);
+        nameRepository.save(name);
         CreateCommand createCommand = new CreateCommand(UUID.randomUUID(), "test name", "test email", CustomerState.INITIALIZED, Instant.now());
         //When
         Try<CreateCommand> validCommand = createCommandValidator.valid(createCommand);
         //Then
-        Assert.assertTrue(validCommand.isFailure());
-        Assert.assertTrue(((BindException) validCommand.getCause()).getAllErrors().contains(nameError));
-        Assert.assertEquals(1, ((BindException) validCommand.getCause()).getFieldErrors().size());
-        //Clean up
-        nameRepository.deleteById(savedName.getId());
+        assertTrue(validCommand.isFailure());
+        assertTrue(((BindException) validCommand.getCause()).getAllErrors().contains(nameError));
+        assertEquals(1, ((BindException) validCommand.getCause()).getFieldErrors().size());
     }
 
 
     @Test
-    @DisplayName("should return error email exists in database")
+    @DisplayName("should return error - email exists in database")
     public void shouldReturnErrorEmailExistsInDatabase() {
         //Given
+        FieldError emailError = new FieldError("CreateCommand", "email", "email exists in database");
         Email email = new Email("test email");
-        Email savedEmail = emailRepository.save(email);
+        emailRepository.save(email);
         CreateCommand createCommand = new CreateCommand(UUID.randomUUID(), "test name", "test email", CustomerState.INITIALIZED, Instant.now());
         //When
         Try<CreateCommand> valid = createCommandValidator.valid(createCommand);
         //Then
-        Assert.assertTrue(valid.isFailure());
-        Assert.assertEquals(1, ((BindException) valid.getCause()).getFieldErrors().size());
-        Assert.assertEquals("email", ((BindException) valid.getCause()).getFieldErrors().get(0).getField());
-        Assert.assertEquals("email exists in database", ((BindException) valid.getCause()).getFieldErrors().get(0).getDefaultMessage());
-        //Clean up
-        emailRepository.deleteById(savedEmail.getId());
+        assertTrue(valid.isFailure());
+        assertEquals(1, ((BindException) valid.getCause()).getFieldErrors().size());
+        assertTrue(((BindException) valid.getCause()).getAllErrors().contains(emailError));
     }
 
     @Test
-    @DisplayName("should return error (letter size) - email exists in database ")
-    public void shouldReturnErrorLetterSizeEmailExistsInDatabase() {
+    @DisplayName("should return error (letters size) - email exists in database ")
+    public void shouldReturnErrorLettersSizeEmailExistsInDatabase() {
         //Given
+        FieldError emailError = new FieldError("CreateCommand", "email", "email exists in database");
         Email email = new Email("TEST@email.com");
-        Email savedEmail = emailRepository.save(email);
+        emailRepository.save(email);
         CreateCommand createCommand = new CreateCommand(UUID.randomUUID(), "test name", "test@email.com", CustomerState.INITIALIZED, Instant.now());
         //When
         Try<CreateCommand> valid = createCommandValidator.valid(createCommand);
         //Then
-        Assert.assertTrue(valid.isFailure());
-        Assert.assertEquals(1, ((BindException) valid.getCause()).getFieldErrors().size());
-        Assert.assertEquals("email", ((BindException) valid.getCause()).getFieldErrors().get(0).getField());
-        Assert.assertEquals("email exists in database", ((BindException) valid.getCause()).getFieldErrors().get(0).getDefaultMessage());
-        //Clean up
-        emailRepository.deleteById(savedEmail.getId());
+        assertTrue(valid.isFailure());
+        assertEquals(1, ((BindException) valid.getCause()).getFieldErrors().size());
+        assertTrue(((BindException) valid.getCause()).getAllErrors().contains(emailError));
     }
 
     @Test
     @DisplayName("should return two errors - email and name exist in database")
     public void shouldReturnTwoErrorsEmailAndNameExistInDatabase() {
         //Given
+        FieldError emailError = new FieldError("CreateCommand", "email", "email exists in database");
+        FieldError nameError = new FieldError("CreateCommand", "name", "name exists in database");
         Name name = new Name("test name");
         Email email = new Email("test email");
-        Name savedName = nameRepository.save(name);
-        Email savedEmail = emailRepository.save(email);
+        nameRepository.save(name);
+        emailRepository.save(email);
         CreateCommand createCommand = new CreateCommand(UUID.randomUUID(), "test name", "test email", CustomerState.INITIALIZED, Instant.now());
         //When
         Try<CreateCommand> valid = createCommandValidator.valid(createCommand);
         //Then
-        Assert.assertTrue(valid.isFailure());
-        Assert.assertEquals(2, ((BindException) valid.getCause()).getFieldErrors().size());
-        Assert.assertEquals("name", ((BindException) valid.getCause()).getFieldErrors().get(0).getField());
-        Assert.assertEquals("email", ((BindException) valid.getCause()).getFieldErrors().get(1).getField());
-        Assert.assertEquals("name exists in database", ((BindException) valid.getCause()).getFieldErrors().get(0).getDefaultMessage());
-        Assert.assertEquals("email exists in database", ((BindException) valid.getCause()).getFieldErrors().get(1).getDefaultMessage());
-        //Clean up
-        nameRepository.deleteById(savedName.getId());
-        emailRepository.deleteById(savedEmail.getId());
+        assertTrue(valid.isFailure());
+        assertEquals(2, ((BindException) valid.getCause()).getFieldErrors().size());
+        assertTrue(((BindException) valid.getCause()).getAllErrors().contains(nameError));
+        assertTrue(((BindException) valid.getCause()).getAllErrors().contains(emailError));
     }
 
     @Test
-    @DisplayName("should return success email and name do not exist in database")
+    @DisplayName("should return success - email and name do not exist in database")
     public void shouldReturnSuccessEmailAndNameDoNotExistInDatabase() {
         //Given
         CreateCommand createCommand = new CreateCommand(UUID.randomUUID(), "test name", "test email", CustomerState.INITIALIZED, Instant.now());
         //When
         Try<CreateCommand> valid = createCommandValidator.valid(createCommand);
         //Then
-        Assert.assertTrue(valid.isSuccess());
+        assertTrue(valid.isSuccess());
     }
 }
