d1667823768f428ed3ea34721718fb30121b3e84
davv1d <kowalskidawid63@gmail.com>
2020-08-05 22:07:20 +0200

    Create ChangeNameCommandValidator

--- src/main/java/com/davv1d/customerevents/validation/ChangeNameCommandValidator.java
+++ src/main/java/com/davv1d/customerevents/validation/ChangeNameCommandValidator.java
@@ -3,6 +3,7 @@ package com.davv1d.customerevents.validation;
 import com.davv1d.customerevents.command.ChangeNameCommand;
 import com.davv1d.customerevents.repository.NameRepository;
 import com.davv1d.customerevents.validation.conditions.Condition;
+import org.springframework.validation.FieldError;
 
 import java.util.Collections;
 import java.util.List;
@@ -16,6 +17,8 @@ public class ChangeNameCommandValidator extends Validator<ChangeNameCommand> {
 
     @Override
     protected List<Condition<ChangeNameCommand>> get(ChangeNameCommand value) {
-        return Collections.emptyList();
+        FieldError nameError = new FieldError("ChangeNameCommand", "name", "name exists in database");
+        Condition<ChangeNameCommand> nameCondition = new Condition<>(value, nameError, command -> !nameRepository.existsByNameIgnoreCase(command.getName()));
+        return Collections.singletonList(nameCondition);
     }
 }
